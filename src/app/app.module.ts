d66a00cca84bc9dd0e652169ddc53b3830e7b481
Alexis Reyes <danzrock86@gmail.com>
2023-02-23 19:14:31 -0600

    update de la Gif app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { GifsModule } from './gifs/gifs.module';
@@ -13,7 +14,8 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
   imports: [
     BrowserModule,
     SharedModule,
-    GifsModule
+    GifsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,25 +1,41 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { SearchGifsResponse, Gif } from '../interfaces/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  private APIkey: string = "ERlNMjb3MEHVjdRqkwYFlHhT3SOHXWHV";
   private _historial: string[] = [];
 
-  get historial(){
-  
-    return[...this._historial];
+  //TO DO: hay que cambiar el any por el tipo correspondiente
+  public resultados: Gif[] = [];
+
+  get historial() {
+
+    return [...this._historial];
   }
 
-  buscarGifs( argumento: string = ''){
+  constructor(private argumento1: HttpClient) { }
+
+  buscarGifs(argumento: string = '') {
     argumento = argumento.trim().toLocaleLowerCase();
-    if( !this._historial.includes(argumento)){
+    if (!this._historial.includes(argumento)) {
       this._historial.unshift(argumento);
 
     }
     //esto cortara el arreglo para que no acepte mas de 10
-    this._historial = this._historial.splice(0,10);
+    this._historial = this._historial.splice(0, 10);
     console.log(this._historial);
+
+    //peticion tipo GET 
+    this.argumento1.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=ERlNMjb3MEHVjdRqkwYFlHhT3SOHXWHV&q=${argumento}&limit=10`)
+      .subscribe((respuesta) => {
+        console.log(respuesta.data);
+        this.resultados = respuesta.data;
+      });
+
   }
 }
